a3ce0529bcb5ecff00a5c8f01384cb03db31bb6b
Ramis <rra88@mail.ru>
2023-01-14 00:24:38 +0300

    приветствие

---
+++ ex006(if)/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "муталиб")
+{
+    Console.WriteLine("Мутя ты что ли.");     
+}
+else
+{
+    Console.Write("Салам,");
+    Console.WriteLine(username);
+}
