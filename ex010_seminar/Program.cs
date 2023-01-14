ad025242e178945866d9b61a910dbbd2bcc6ef45
Ramis <rra88@mail.ru>
2023-01-14 18:47:10 +0300

    zadacha

---
+++ ex010_seminar/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Является ли число 1 квадратом числа 2");
+Console.WriteLine("Введите число 1");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число 2");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x==y*y)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
