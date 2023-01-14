e02a99652e8226c844a11d31870d94fd6ea1c684
Ramis <rra88@mail.ru>
2023-01-14 22:11:29 +0300

    dz1.2.3.4

---
+++ ex012_dz1/Program.cs
@@ -0,0 +1,51 @@
+﻿// Console.WriteLine("Введите число 1");
+// int x = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число 2");
+// int y = Convert.ToInt32(Console.ReadLine());
+// if(x>y) Console.WriteLine("max"+ x);
+// else Console.WriteLine("max"+ y);
+
+
+
+
+
+
+
+// Console.WriteLine("Введите число 1");
+// int x = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число 2");
+// int y = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число 3");
+// int z = Convert.ToInt32(Console.ReadLine());
+// int max = 0;
+
+// if(x>y && x>z ) max=x; 
+// else if(y>x && y>z ) max=y;
+// else if(z>x && z>y ) max=z;
+// Console.WriteLine("наибольшее число:"+max);
+
+
+
+
+
+
+// Console.WriteLine("Введите число кратное двум");
+// int x = Convert.ToInt32(Console.ReadLine());
+
+// if(x % 2 == 0) Console.WriteLine("Это четное число");
+// else Console.WriteLine("Это нечетное число");
+
+
+
+
+Console.WriteLine("Введите целое число");
+int x = Convert.ToInt32(Console.ReadLine());
+int z= 2;
+while(z<=x)
+{
+    Console.WriteLine(z+"");
+    z=z+2;
+}
